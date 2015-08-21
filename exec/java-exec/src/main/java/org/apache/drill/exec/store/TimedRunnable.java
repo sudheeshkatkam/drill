599064caa14750f448812a269fc80a0dca69a346
Sudheesh Katkam <skatkam@maprtech.com>
2015-08-21 15:32:45 -0700

    more

--- exec/java-exec/src/main/java/org/apache/drill/exec/store/TimedRunnable.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/store/TimedRunnable.java
@@ -20,12 +20,9 @@ package org.apache.drill.exec.store;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 import org.apache.drill.common.concurrent.ExtendedLatch;
 import org.apache.drill.common.exceptions.UserException;
@@ -37,6 +34,7 @@ import com.google.common.collect.Lists;
 /**
  * Class used to allow parallel executions of tasks in a simplified way. Also maintains and reports timings of task completion.
  * TODO: look at switching to fork join.
+ *
  * @param <V> The time value that will be returned when the task is executed.
  */
 public abstract class TimedRunnable<V> implements Runnable {
@@ -51,31 +49,33 @@ public abstract class TimedRunnable<V> implements Runnable {
   @Override
   public final void run() {
     long start = System.nanoTime();
-    threadStart=start;
-    try{
+    threadStart = start;
+    try {
       value = runInner();
-    }catch(Exception e){
+    } catch (Exception e) {
       this.e = e;
-    }finally{
+    } finally {
       timeNanos = System.nanoTime() - start;
     }
   }
 
-  protected abstract V runInner() throws Exception ;
+  protected abstract V runInner() throws Exception;
+
   protected abstract IOException convertToIOException(Exception e);
 
-  public long getThreadStart(){
+  public long getThreadStart() {
     return threadStart;
   }
-  public long getTimeSpentNanos(){
+
+  public long getTimeSpentNanos() {
     return timeNanos;
   }
 
   public final V getValue() throws IOException {
-    if(e != null){
-      if(e instanceof IOException){
+    if (e != null) {
+      if (e instanceof IOException) {
         throw (IOException) e;
-      }else{
+      } else {
         throw convertToIOException(e);
       }
     }
@@ -87,16 +87,16 @@ public abstract class TimedRunnable<V> implements Runnable {
     final CountDownLatch latch;
     final Runnable runnable;
 
-    public LatchedRunnable(CountDownLatch latch, Runnable runnable){
+    public LatchedRunnable(CountDownLatch latch, Runnable runnable) {
       this.latch = latch;
       this.runnable = runnable;
     }
 
     @Override
     public void run() {
-      try{
+      try {
         runnable.run();
-      }finally{
+      } finally {
         latch.countDown();
       }
     }
@@ -106,30 +106,40 @@ public abstract class TimedRunnable<V> implements Runnable {
    * Execute the list of runnables with the given parallelization.  At end, return values and report completion time
    * stats to provided logger. Each runnable is allowed a certain timeout. If the timeout exceeds, existing/pending
    * tasks will be cancelled and a {@link UserException} is thrown.
-   * @param activity Name of activity for reporting in logger.
-   * @param logger The logger to use to report results.
-   * @param runnables List of runnables that should be executed and timed.  If this list has one item, task will be
-   *                  completed in-thread. Runnable must handle {@link InterruptedException}s.
-   * @param parallelism  The number of threads that should be run to complete this task.
+   *
+   * @param activity    Name of activity for reporting in logger.
+   * @param logger      The logger to use to report results.
+   * @param runnables   List of runnables that should be executed and timed.  If this list has one item, task will be
+   *                    completed in-thread. Runnable must handle {@link InterruptedException}s.
+   * @param parallelism The number of threads that should be run to complete this task.
    * @return The list of outcome objects.
    * @throws IOException All exceptions are coerced to IOException since this was build for storage system tasks initially.
    */
   public static <V> List<V> run(final String activity, final Logger logger, final List<TimedRunnable<V>> runnables, int parallelism) throws IOException {
     Stopwatch watch = new Stopwatch().start();
-    long timedRunnableStart=System.nanoTime();
-    if(runnables.size() == 1){
+    long timedRunnableStart = System.nanoTime();
+    if (runnables.size() == 1) {
       parallelism = 1;
       runnables.get(0).run();
-    }else{
-      parallelism = Math.min(parallelism,  runnables.size());
+    } else {
+      parallelism = Math.min(parallelism, runnables.size());
       final ExtendedLatch latch = new ExtendedLatch(runnables.size());
       final ExecutorService threadPool = Executors.newFixedThreadPool(parallelism);
-      try{
-        for(TimedRunnable<V> runnable : runnables){
-          threadPool.submit(new LatchedRunnable(latch, runnable));
+      try {
+        for (final TimedRunnable<V> runnable : runnables) {
+          threadPool.submit(new Runnable() {
+            @Override
+            public void run() {
+              try {
+                runnable.run();
+              } finally {
+                latch.countDown();
+              }
+            }
+          });
         }
 
-        final long timeout = (long)Math.ceil((TIMEOUT_PER_RUNNABLE_IN_MSECS * runnables.size())/parallelism);
+        final long timeout = (long) Math.ceil((TIMEOUT_PER_RUNNABLE_IN_MSECS * runnables.size()) / parallelism);
         if (!latch.awaitUninterruptibly(timeout)) {
           // Issue a shutdown request. This will cause existing threads to interrupt and pending threads to cancel.
           // It is highly important that the task Runnables are handling interrupts correctly.
@@ -146,13 +156,14 @@ public abstract class TimedRunnable<V> implements Runnable {
             logger.warn("Interrupted while waiting for pending threads in activity '{}' to terminate.", activity);
           }
 
-          final String errMsg = String.format("Waited for %d ms, but tasks for '%s' are not complete. " +
-              "Total runnable size %d, parallelism %d. Number of tasks that never start: %d. %s.", timeout, activity,
-            runnables.size(), parallelism, neverStartedList.size(), terminated ? "Terminated gracefully." : "Terminated due to timeout.");
+          final String errMsg = String.format("Waited for %d ms (elapsed: %s), but %d tasks for '%s' are not complete. " +
+              "Total runnable size %d, parallelism %d. Number of tasks that never start: %d. %s.",
+            timeout, watch, parallelism - latch.getCount(), activity, runnables.size(), parallelism,
+            neverStartedList.size(), terminated ? "Terminated gracefully" : "Terminated due to timeout (5 s)");
           logger.error(errMsg);
           throw UserException.resourceError()
-              .message(errMsg)
-              .build(logger);
+            .message(errMsg)
+            .build(logger);
         }
       } finally {
         if (!threadPool.isShutdown()) {
@@ -166,43 +177,43 @@ public abstract class TimedRunnable<V> implements Runnable {
     long max = 0;
     long count = 0;
     // measure thread creation times
-    long earliestStart=Long.MAX_VALUE;
-    long latestStart=0;
-    long totalStart=0;
+    long earliestStart = Long.MAX_VALUE;
+    long latestStart = 0;
+    long totalStart = 0;
     IOException excep = null;
-    for(final TimedRunnable<V> reader : runnables){
-      try{
+    for (final TimedRunnable<V> reader : runnables) {
+      try {
         values.add(reader.getValue());
         sum += reader.getTimeSpentNanos();
         count++;
         max = Math.max(max, reader.getTimeSpentNanos());
-        earliestStart=Math.min(earliestStart, reader.getThreadStart() - timedRunnableStart);
-        latestStart=Math.max(latestStart, reader.getThreadStart()-timedRunnableStart);
-        totalStart+=latestStart=Math.max(latestStart, reader.getThreadStart()-timedRunnableStart);
-      }catch(IOException e){
-        if(excep == null){
+        earliestStart = Math.min(earliestStart, reader.getThreadStart() - timedRunnableStart);
+        latestStart = Math.max(latestStart, reader.getThreadStart() - timedRunnableStart);
+        totalStart += latestStart = Math.max(latestStart, reader.getThreadStart() - timedRunnableStart);
+      } catch (IOException e) {
+        if (excep == null) {
           excep = e;
-        }else{
+        } else {
           excep.addSuppressed(e);
         }
       }
     }
 
-    if(logger.isInfoEnabled()){
-      double avg = (sum/1000.0/1000.0)/(count*1.0d);
-      double avgStart = (totalStart/1000.0)/(count*1.0d);
+    if (logger.isInfoEnabled()) {
+      double avg = (sum / 1000.0 / 1000.0) / (count * 1.0d);
+      double avgStart = (totalStart / 1000.0) / (count * 1.0d);
 
       logger.info(
-          String.format("%s: Executed %d out of %d using %d threads. "
-              + "Time: %dms total, %fms avg, %dms max.",
-              activity, count, runnables.size(), parallelism, watch.elapsed(TimeUnit.MILLISECONDS), avg, max/1000/1000));
+        String.format("%s: Executed %d out of %d using %d threads. "
+            + "Time: %dms total, %fms avg, %dms max.",
+          activity, count, runnables.size(), parallelism, watch.elapsed(TimeUnit.MILLISECONDS), avg, max / 1000 / 1000));
       logger.info(
-              String.format("%s: Executed %d out of %d using %d threads. "
-                              + "Earliest start: %f \u03BCs, Latest start: %f \u03BCs, Average start: %f \u03BCs .",
-                      activity, count, runnables.size(), parallelism, earliestStart/1000.0, latestStart/1000.0, avgStart));
+        String.format("%s: Executed %d out of %d using %d threads. "
+            + "Earliest start: %f \u03BCs, Latest start: %f \u03BCs, Average start: %f \u03BCs .",
+          activity, count, runnables.size(), parallelism, earliestStart / 1000.0, latestStart / 1000.0, avgStart));
     }
 
-    if(excep != null) {
+    if (excep != null) {
       throw excep;
     }
 
